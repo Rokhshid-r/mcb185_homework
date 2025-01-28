89e2045c583f25d278439ab94ac7755108c55c52
Rokhshid-r <rokhshidrafiei@gmail.com>
2025-01-28 15:18:23 -0800

    new

--- 12phred.py
+++ 12phred.py
@@ -1,8 +1,15 @@
 
+import math
 def char_to_prob(char:str):
-        return ord(char)
-def prob_to_char(prob:float):
-    return chr(prob)
-
+    error_rate_num = ord(char) - 33
+    error_rate = 10**(-1*error_rate_num/10)
+    return error_rate
+def prob_to_char(error_rate:float):
+    num = -1 * math.log(error_rate, 10)
+    ascii = 10 * math.ceil(num) + 33
+    character = chr(int(ascii))
+    return character
 print(char_to_prob('A'))
-print(prob_to_char(0.001))
+print(char_to_prob('%'))
+print(char_to_prob('!'))
+print(prob_to_char(.001))
