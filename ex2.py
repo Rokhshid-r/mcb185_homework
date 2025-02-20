01960089430738e28756434c2f01f01db602b916
Rokhshid-r <rokhshidrafiei@gmail.com>
2025-02-20 15:34:27 -0800

    new

--- ex2.py
+++ ex2.py
@@ -1,3 +1,15 @@
 
-file
+import random
+for i in range(100):
+    above = 0
+    below = 0
+    a = random.uniform(-100, 100)
+    b = random.uniform(-100, 100)
+    if b >= a**2:
+        above = above + 1
+    else:
+        below = below + 1
+pi = above/(above + below)
+print (pi)
+
 
